d47c7f4e7dfc2ebd832d090f0dfb7fb800310807
BelCarrillo <56990269+BelCarrillo@users.noreply.github.com>
2019-10-25 15:52:41 -0500

    Add files via upload

---
+++ 03_personal-info.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 19 12:20:19 2019
+
+@author: CEC
+"""
+
+nombre=input("Cuál es tu primer nombre? ")
+apellido=input("Apellido? ")
+localidad=input("De dónde eres? ")
+edad=input("Cuántos años tienes? ")
+space=" "
+print("Hola "+nombre+space+apellido+space+"de"+space+localidad+space+"y"+space+edad+" años")
